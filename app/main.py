d6d3f1076eb8e76b25a179716ada980582d8818d
soulja <gadzimuradumraov0410@gmail.com>
2026-06-25 16:32:40 +0300

    Added notification router to main.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from .notifications.routers.notifications import router as notification_order
+
 app = FastAPI(
     title="Gorbushka Keepers Ozon",
     version="1.0.0",
 )
 
+app.include_router(notification_order)
+
+
+@app.get("/")
+def ping():
+    return {"message": "OK"}
